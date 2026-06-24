04b253f6de9b77522390248e78576986daf257d1
althotdev <althotdev@outlook.com>
2026-06-23 20:40:21 -0400

    Move IFO loading to class

--- nethomevideo/DVD.cpp
+++ nethomevideo/DVD.cpp
@@ -19,21 +19,14 @@ void FillOutVideoTS(VideoTSInfo* info, std::string path) {
 			info->hasVMG = true;
 
 			// read vmg
-			const size_t vmgSize = fs::file_size(vmgPath);
-			char* ifoContents = (char*)malloc(vmgSize);
-			std::ifstream in(vmgPath);
-			in.read(ifoContents, vmgSize);
-			in.close();
+			info->vmg = new IFO(vmgPath);
 
 			// assume little endian
 			unsigned short titleSetFromVMG;
-			memcpy(&titleSetFromVMG, ifoContents + 0x003e, 2);
+			memcpy(&titleSetFromVMG, info->vmg->getIfoContents() + 0x003e, 2);
 			titleSetFromVMG = _byteswap_ushort(titleSetFromVMG);
 			
 			info->titleSetCount = (int)titleSetFromVMG;
-
-			// discard vmg
-			free(ifoContents);
 
 		}
 		else {
@@ -129,4 +122,94 @@ DVDDirectoryLayout* getLayoutInfo(std::string dvdPath) {
 	}
 
 	return layout;
+}
+
+IFO::IFO(std::string path)
+{
+	namespace fs = std::filesystem;
+
+	if (!fs::exists(path)) {
+		throw std::out_of_range(
+			std::format("{} does not exist.", path)
+		);
+	}
+
+	if (!fs::is_regular_file(path)) {
+		throw std::out_of_range(
+			std::format(
+				"{} is not a regular file. Could be a directory or a device.", 
+				path
+			)
+		);
+	}
+
+	// get file size and allocate
+	const size_t fileSize = fs::file_size(path);
+	if (fileSize < 0x03D8 + 40) {
+		throw std::out_of_range(
+			std::format(
+				"File {} too small to be a valid IFO file.",
+				path
+			)
+		);
+	}
+
+	ifoContents = (char*)malloc(fileSize);
+	
+	if (ifoContents == NULL) {
+		throw std::runtime_error("malloc failed");
+	}
+
+	// grab data from file and close as quickly as possible
+	std::ifstream in(path);
+	in.read(ifoContents, fileSize); in.close();
+
+	// validate
+	constexpr char ifoMagic[9] = 
+		{ 'D', 'V', 'D', 'V', 'I', 'D', 'E', 'O', '-' };
+
+	constexpr char vmgMagic[3] = { 'V', 'M', 'G' };
+	constexpr char vtsMagic[3] = { 'V', 'T', 'S' };
+
+	if (memcmp(ifoMagic, ifoContents, 9) != 0) {
+		throw std::logic_error(
+			std::format(
+				"File {} isn't a valid DVD-Video IFO file...",
+				path
+			)
+		);
+	}
+
+
+	type = IFO_INVALID;
+
+	if (memcmp(vmgMagic, ifoContents + 9, 3) == 0) type = IFO_VMG;
+	if (memcmp(vtsMagic, ifoContents + 9, 3) == 0) type = IFO_VTS;
+
+	if (type == IFO_INVALID) {
+		throw std::logic_error(
+			std::format(
+				"File {} is unsupported (neither VMG or VTS IFO)",
+				path
+			)
+		);
+	}
+
+}
+
+IFOType IFO::getType()
+{
+	return type;
+}
+
+char* IFO::getIfoContents()
+{
+	return ifoContents;
+}
+
+IFO::~IFO()
+{
+	if (ifoContents != nullptr && ifoContents != NULL) {
+		free(ifoContents);
+	}
 }
--- nethomevideo/DVD.h
+++ nethomevideo/DVD.h
@@ -7,6 +7,27 @@
 #include <iostream> // for std::string
 
 
+enum IFOType : char {
+	IFO_INVALID,
+	IFO_VMG, // VIDEO_TS.IFO
+	IFO_VTS // VTS_xx_0.IFO
+};
+
+class IFO {
+	IFOType type;
+	char* ifoContents = nullptr;
+public:
+	[[deprecated("Use IFO(std::string path) instead")]]
+	IFO() { throw std::runtime_error("default IFO ctor"); }
+	IFO(std::string path);
+	
+	IFOType getType();
+	char* getIfoContents();
+
+	~IFO();
+};
+
+
 // info about the AUDIO_TS folder
 struct AudioTSInfo {
 	bool exists = false;
@@ -17,6 +38,7 @@ struct VideoTSInfo {
 	bool exists = false;
 	bool hasVMG = false; // checks for VIDEO_TS/VIDEO_TS.IFO
 	int titleSetCount = 0;
+	IFO *vmg;
 };
 
 // info about the DVD's directory's layout
